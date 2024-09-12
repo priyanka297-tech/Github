08bd52fadcbb298ee212a2ef40a7ed54326197e7
priyanka297-tech <priyankapal29702@gmail.com>
2024-09-12 11:54:48 +0530

    commit

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
 print("hello world")
+a=50000
+b=75000
+c=60000
+total=a+b+c
+print("total=",total)
+a=type(total)
+print(a)
+
+password= "Secure123"
+print(password)
+print(type(password))
+
+
+
+
